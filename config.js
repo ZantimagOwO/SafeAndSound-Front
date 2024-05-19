4be1900ca33f78a77513df167cddbd71445a4023
Santiago Gimeno Julián <85684226+ZantimagOwO@users.noreply.github.com>
2024-05-19 20:21:22 +0200

    .

--- config.js
+++ config.js
@@ -3,9 +3,7 @@ const configs = {
   mario: "http://192.168.0.16:3000",
   santi: "http://192.168.1.132:3000",
 };
-  
-  const activeConfig = 'mario'; 
-  
-  export const serverIP = configs[activeConfig];
-  
-  
+
+const activeConfig = 'mario'; 
+
+export const serverIP = configs[activeConfig];
