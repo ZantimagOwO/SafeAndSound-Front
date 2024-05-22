0be5f42d74ab49e341461ab30b1f86a191988508
Santiago Gimeno Julián <85684226+ZantimagOwO@users.noreply.github.com>
2024-05-22 19:44:41 +0200

    widget exists with library

--- App.jsx
+++ App.jsx
@@ -20,10 +20,18 @@ import { Linking, NativeModules } from "react-native";
 import * as Contacts from "expo-contacts";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { registerRootComponent } from "expo";
+import {
+  registerWidgetConfigurationScreen,
+  registerWidgetTaskHandler,
+} from "react-native-android-widget";
+import { HelloWidgetPreviewScreen } from './src/widget/Widgetpreview';
+import { widgetTaskHandler } from './src/widget/widget-task-handler';
 
 const Stack = createNativeStackNavigator();
 
 AppRegistry.registerComponent("main", () => App);
+registerRootComponent(App)
+registerWidgetTaskHandler(widgetTaskHandler);
 
 export default function App() {
 
@@ -63,22 +71,37 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator
           screenOptions={{ headerShown: false }}
-          initialRouteName='Main'
+          initialRouteName="Main"
         >
-          <Stack.Screen name='Login' component={Login} />
-          <Stack.Screen name='Signup' component={Signup} />
-          <Stack.Screen name='InformacionInicial' component={InformacionInicial}/>
-          <Stack.Screen name='Main' component={Main} />
-          <Stack.Screen name='Boton' component={Boton} />
-          <Stack.Screen name='ProtegidosProtectores' component={ProtegidosProtectores}/>
-          <Stack.Screen name='InformacionLegal' component={InformacionLegal} />
-          <Stack.Screen name='Agenda' component={Agenda} />
-          <Stack.Screen name='Supervivencia' component={Supervivencia} />
-          <Stack.Screen name='Wiki' component={Wiki} />
-          <Stack.Screen name='CentrosSalud' component={CentrosSalud} />
-          <Stack.Screen name='Medinator' component={Medinator} />
-          <Stack.Screen name='InformacionPersonal' component={InformacionPersonal}/>
-          <Stack.Screen name='InformacionPersonalEdit' component={InformacionPersonalEdit}/>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Signup" component={Signup} />
+          <Stack.Screen
+            name="InformacionInicial"
+            component={InformacionInicial}
+          />
+          <Stack.Screen name="Main" component={Main} />
+          <Stack.Screen name="Boton" component={Boton} />
+          <Stack.Screen
+            name="ProtegidosProtectores"
+            component={ProtegidosProtectores}
+          />
+          <Stack.Screen name="InformacionLegal" component={InformacionLegal} />
+          <Stack.Screen name="Agenda" component={Agenda} />
+          <Stack.Screen
+            name="Supervivencia"
+            component={HelloWidgetPreviewScreen}
+          />
+          <Stack.Screen name="Wiki" component={Wiki} />
+          <Stack.Screen name="CentrosSalud" component={CentrosSalud} />
+          <Stack.Screen name="Medinator" component={Medinator} />
+          <Stack.Screen
+            name="InformacionPersonal"
+            component={InformacionPersonal}
+          />
+          <Stack.Screen
+            name="InformacionPersonalEdit"
+            component={InformacionPersonalEdit}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </View>
---
+++ src/widget/Widgetpreview.jsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { StyleSheet, View } from "react-native";
+import { WidgetPreview } from "react-native-android-widget";
+import { HelloWidget } from "./widget";
+
+export function HelloWidgetPreviewScreen() {
+  return (
+    <View style={styles.container}>
+      <WidgetPreview
+        renderWidget={() => <HelloWidget />}
+        width={320}
+        height={200}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
