f539b22dbd5a46371e4059d49b8637a365ba4e17
Santiago Gimeno Julián <85684226+ZantimagOwO@users.noreply.github.com>
2024-05-24 00:11:37 +0200

    works without context

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,43 +1,80 @@
-import { Stack } from "expo-router";
+import React, { useEffect } from "react";
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Main from "./src/pages/Main";
+import Boton from "./src/pages/BotonEmergencia/Boton";
 import ProtegidosProtectores from "./src/pages/ProtegidosProtectores/ProtegidosProtectores";
-import { View } from "react-native-reanimated/lib/typescript/Animated";
+import InformacionLegal from "./src/pages/InformacionLegal";
+import Agenda from "./src/pages/Agenda";
+import Supervivencia from "./src/pages/Supervivencia";
+import Wiki from "./src/pages/Wiki";
+import CentrosSalud from "./src/pages/CentrosSalud";
+import Medinator from "./src/pages/Medinator";
+import InformacionPersonal from "./src/pages/InformacionPersonal/InformacionPersonal";
+import InformacionPersonalEdit from "./src/pages/InformacionPersonal/InformacionPersonalEdit";
+import InformacionInicial from "./src/pages/Login-Signup/InformacionInicial";
+import Login from "./src/pages/Login-Signup/Login";
+import Signup from "./src/pages/Login-Signup/Signup";
+
+const Stack = createNativeStackNavigator();
 
 export default function RootLayout() {
   return (
-    <View style={styles.body}>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{ headerShown: false }}
-          initialRouteName="Main"
-        >
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Signup" component={Signup} />
-          <Stack.Screen
-            name="InformacionInicial"
-            component={InformacionInicial}
-          />
-          <Stack.Screen name="Main" component={Main} />
-          <Stack.Screen name="Boton" component={Boton} />
-          <Stack.Screen
-            name="ProtegidosProtectores"
-            component={ProtegidosProtectores}
-          />
-          <Stack.Screen name="InformacionLegal" component={InformacionLegal} />
-          <Stack.Screen name="Agenda" component={Agenda} />
-          <Stack.Screen name="Supervivencia" component={Supervivencia} />
-          <Stack.Screen name="Wiki" component={Wiki} />
-          <Stack.Screen name="CentrosSalud" component={CentrosSalud} />
-          <Stack.Screen name="Medinator" component={Medinator} />
-          <Stack.Screen
-            name="InformacionPersonal"
-            component={InformacionPersonal}
-          />
-          <Stack.Screen
-            name="InformacionPersonalEdit"
-            component={InformacionPersonalEdit}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </View>
+    <Text>Hello</Text>
+    // <Stack>
+    //   <Stack.Screen name="index" />
+    // </Stack>
+    //   <View style={styles.body}>
+    //     <NavigationContainer>
+    //       <Stack.Navigator
+    //         screenOptions={{ headerShown: false }}
+    //         initialRouteName="Main"
+    //       >
+    //         <Stack.Screen name="Login" component={Login} />
+    //         <Stack.Screen name="Signup" component={Signup} />
+    //         <Stack.Screen
+    //           name="InformacionInicial"
+    //           component={InformacionInicial}
+    //         />
+    //         <Stack.Screen name="Main" component={Main} />
+    //         <Stack.Screen name="Boton" component={Boton} />
+    //         <Stack.Screen
+    //           name="ProtegidosProtectores"
+    //           component={ProtegidosProtectores}
+    //         />
+    //         <Stack.Screen name="InformacionLegal" component={InformacionLegal} />
+    //         <Stack.Screen name="Agenda" component={Agenda} />
+    //         <Stack.Screen name="Supervivencia" component={Supervivencia} />
+    //         <Stack.Screen name="Wiki" component={Wiki} />
+    //         <Stack.Screen name="CentrosSalud" component={CentrosSalud} />
+    //         <Stack.Screen name="Medinator" component={Medinator} />
+    //         <Stack.Screen
+    //           name="InformacionPersonal"
+    //           component={InformacionPersonal}
+    //         />
+    //         <Stack.Screen
+    //           name="InformacionPersonalEdit"
+    //           component={InformacionPersonalEdit}
+    //         />
+    //       </Stack.Navigator>
+    //     </NavigationContainer>
+    //   </View>
   );
 }
+
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+  },
+  temp: {
+    position: "absolute",
+    top: 50,
+  },
+});
--- app/src/components/headers/RegularHeader.jsx
+++ app/src/components/headers/RegularHeader.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Constants from 'expo-constants';
 import { TouchableOpacity, StyleSheet, Text, View, Image } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
