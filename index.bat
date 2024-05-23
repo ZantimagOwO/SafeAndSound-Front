efd850ccac21cdb1a3431872ff854572cdcc33e3
Santiago Gimeno Julián <85684226+ZantimagOwO@users.noreply.github.com>
2024-05-23 23:11:44 +0200

    tomar por saco

--- index.bat
+++
@@ -1,2 +0,0 @@
-mkdir "./android/app/src/main/assets"
-copy NUL "./android/app/src/main/assets/index.android.bundle"
--- src/StyleConstants.jsx
+++
@@ -1,6 +0,0 @@
-const StyleConstants = {
-  mainColor: "#68C699",
-  font: "normal", // Cambiar por Inter usando expo-fonts
-};
-
-export default StyleConstants;
