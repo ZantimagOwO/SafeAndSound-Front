e5675679394272b26583468248f16994ff485045
Santiago Gimeno Julián <85684226+ZantimagOwO@users.noreply.github.com>
2024-05-27 12:29:37 +0200

    widget connected with android

---
+++ android/app/src/main/java/com/zantimago/SafeAndSound/CallActivity.java
@@ -0,0 +1,62 @@
+package com.zantimago.SafeAndSound;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class CallActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CALL_PHONE = 1;
+    private String phoneNumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.i("CallActivity - onCreate", "Entrando");
+
+        phoneNumber = "111-333-222-4"; // Número de teléfono a llamar
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            Log.i("CallActivity - onCreate", "NO SE TIENE PERMISO, PIDIENDO...");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL_PHONE);
+        } else {
+            makePhoneCall();
+        }
+    }
+
+    private void makePhoneCall() {
+        Log.i("CallActivity - makePhoneCall", "Entrando");
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:" + phoneNumber));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+            Log.i("CallActivity - makePhoneCall", "Se tienen permisos, haciendo callIntent...");
+            startActivity(callIntent);
+            finish(); // Cierra la actividad después de iniciar la llamada
+        }
+        Log.e("CallActivity - makePhoneCall", "NO SE TIENEN PERMISOS");
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.i("CallActivity - onRequestPermissionsResult", "Entrando");
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CALL_PHONE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                makePhoneCall();
+            } else {
+                // Permiso denegado, muestra un mensaje al usuario
+                finish(); // Cierra la actividad si el permiso es denegado
+            }
+        }
+    }
+}
--- android/app/src/main/java/com/zantimago/SafeAndSound/Widget1.java
+++ android/app/src/main/java/com/zantimago/SafeAndSound/Widget1.java
@@ -1,5 +1,6 @@
 package com.zantimago.SafeAndSound;
 
+import android.Manifest;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
@@ -9,6 +10,8 @@ import android.net.Uri;
 import android.util.Log;
 import android.widget.RemoteViews;
 import androidx.core.app.ActivityCompat;
+
+import java.util.Arrays;
 
 public class Widget1 extends AppWidgetProvider {
 
@@ -22,12 +25,12 @@ public class Widget1 extends AppWidgetProvider {
         // Set up the intent that starts the Phone Call
         Intent intent = new Intent(context, Widget1.class);
         intent.setAction(ACTION_CALL);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, appWidgetId, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, appWidgetId, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
 
         // Register the onClickListener with the PendingIntent
-        views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
+        views.setOnClickPendingIntent(R.id.widget_container, pendingIntent);
 
-        Log.i("Widget", views.toString());
+        Log.i("updateAppWidget", views.toString());
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -35,18 +38,10 @@ public class Widget1 extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget1);
+
+        Log.i("onUpdate", Arrays.toString(appWidgetIds));
 
         for (int appWidgetId : appWidgetIds) {
-
-            // Set up the intent that starts the Phone Call
-            Intent intent = new Intent(context, Widget1.class);
-            intent.setAction(ACTION_CALL);
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, appWidgetId, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-            // Register the onClickListener with the PendingIntent
-            views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
-
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
     }
@@ -54,16 +49,23 @@ public class Widget1 extends AppWidgetProvider {
     @Override
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
+        Log.i("onReceive", intent.toString());
+
         if (ACTION_CALL.equals(intent.getAction())) {
+            Log.i("onReceive", "Entrando en ACTION_CALL...");
             String phoneNumber = "111-333-222-4";
             Intent callIntent = new Intent(Intent.ACTION_CALL);
             callIntent.setData(Uri.parse("tel:" + phoneNumber));
             callIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            Log.i("onReceive", "Intent: " + callIntent);
             if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.CALL_PHONE) == android.content.pm.PackageManager.PERMISSION_GRANTED) {
+                Log.i("onReceive", "Permisos concedidos...");
                 context.getApplicationContext().startActivity(callIntent);
             } else {
                 // Handle the case where the permission is not granted
                 // You may want to request the permission here
+                Log.e("onReceive", "FALTAN PERMISOS PARA REALIZAR LA LLAMADA!");
+                //ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, 1);
             }
         }
     }
