ffdda76f0a545b81d2d900b552f8dd98225b09e6
Santiago Gimeno Julián <85684226+ZantimagOwO@users.noreply.github.com>
2024-06-02 14:25:20 +0200

    add protector now sends sms to th phone insted of santi

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const configs = {
   mario: "http://192.168.0.16:3000",
   santi: "http://192.168.1.132:3000",
-  AWS: "http://myeventz.es:3000",
+  AWS: "https://myeventz.es:3000",
 };
 
 const activeConfig = 'santi'; 
